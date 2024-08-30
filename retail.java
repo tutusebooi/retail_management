d014be34ef6556c21cbc53d1b0d96e37bcd4cc8d
46910891922k23 <xbooi023@student.wethinkcode.co.za>
2024-08-30 13:38:32 +0200

    day6

--- retail.java
+++ retail.java
@@ -1,9 +1,47 @@
+import javax.print.DocFlavor.STRING;
+
 /**
  * retail
+ * potentially be part of a larger retail management system where products are handled
  */
 public class retail {
 
     public static void main(String[] args) {
         
     }
+
+    public String milk(String milk){
+        return milk;
+    }
+    public String Yogurt(String Yogurt){
+        return Yogurt;
+
+    }
+    public String Chocolate(String Chocolate){
+        return Chocolate;
+    }
+
+    public String infantFomular(String infantFomular){
+        return infantFomular;
+    }
+
+    public String Cheese(String Cheese){
+        return Cheese;
+    }
+    public String Beef(String Beef){
+        return Beef;
+    }
+    public String Pork(String Pork){
+        return Pork;
+    }
+    public String Ham(String Ham){
+        return Ham;
+    }
+    public String Lamb(String lamb){
+        return lamb;
+    }
+    public String Goat(String Goat){
+        return Goat;
+    }
+
 }
